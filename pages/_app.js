e2c79113b8d9aa605ebc7801c0780c798c3da9ff
DavidWaza <wazabanga00@gmail.com>
2022-11-30 19:30:17 +0100

    created a background body image

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../styles/Home.module.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
