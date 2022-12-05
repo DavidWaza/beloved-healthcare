d78bfafd98c004887c086b97b3eb5f63cdd008b4
DavidWaza <wazabanga00@gmail.com>
2022-12-05 11:16:15 +0100

    added the form validation to appointment module

--- component/Appointment/appointment.js
+++ component/Appointment/appointment.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import styles from "../../styles/Home.module.css";
-import Button from "../Button/button";
+import Contstyles from "../../styles/Contact.module.css";
+import { useForm, ValidationError } from "@formspree/react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Appointment = () => {
+  const [state, handleSubmit] = useForm("mzbwnzqa");
+  if (state.succeeded) {
+    return (
+      <div className={Contstyles.successContainer}>
+        <div className={Contstyles.successContent}>
+          <Image src="/shake.gif" alt="gif" width={100} height={100} />
+        </div>
+        <div className={Contstyles.success}>
+          <p>Congratulations!!</p>
+          <p>Your Appointment has been recieved</p>
+        </div>
+        <Link href="/">Home</Link>
+      </div>
+    );
+  }
   return (
     <>
       <div>
@@ -21,20 +39,44 @@ const Appointment = () => {
               <div className={styles.appointWrapper}>
                 <div className={styles.formWrapper}>
                   <div className={styles.formControl}>
-                    <form>
-                      <input type="text" placeholder="Name" />
-                      <input type="text" placeholder="Email" />
-                      <input type="number" placeholder="Number" />
-                    </form>
-                    <form>
-                      <input type="text" placeholder="Select service" />
+                    <form onSubmit={handleSubmit}>
+                      <input type="text" placeholder="Name" name="name" />
+                      <ValidationError
+                        prefix="Name"
+                        field="name"
+                        errors={state.errors}
+                      />
+                      <input type="text" placeholder="Email" name="email" />
+                      <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                      />
+                      <input type="text" placeholder="Number" name="number" />
+                      <ValidationError
+                        prefix="Number"
+                        field="number"
+                        errors={state.errors}
+                      />
+                      <input
+                        type="text"
+                        placeholder="Select service"
+                        name="services"
+                      />
+                      <ValidationError
+                        prefix="Service"
+                        field="service"
+                        errors={state.errors}
+                      />
+                      <button
+                        className={styles.appointButton}
+                        type="submit"
+                        disabled={state.submitting}
+                      >
+                        Make an Appointment
+                      </button>
                     </form>
                   </div>
-                </div>
-                <div className={styles.appointButtonContainer}>
-                  <button className={styles.appointButton}>
-                    Make an Appointment
-                  </button>
                 </div>
               </div>
             </Col>
