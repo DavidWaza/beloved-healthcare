b122e05fe8bff08560e576429b8a85a2ec45f292
DavidWaza <wazabanga00@gmail.com>
2022-12-05 16:01:15 +0100

    a little tweak

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
+import { Suspense } from "react";
 const AboutCare = dynamic(() => import("../component/AboutCare/aboutCare"));
 const Care = dynamic(() => import("../component/CareModule/care"));
 const Features = dynamic(() => import("../component/Features/features"));
@@ -12,20 +13,22 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Beloved HealthCare</title>
-        <meta
-          name="description"
-          content="Home service healthcare, home service"
-        />
-      </Head>
-      <Herobanner />
-      <Care />
-      <Service />
-      <AboutCare />
-      <Features />
-      <Appointment />
-    </div>
+    <Suspense fallback={`loading..`}>
+      <div className={styles.container}>
+        <Head>
+          <title>Beloved HealthCare</title>
+          <meta
+            name="description"
+            content="Home service healthcare, home service"
+          />
+        </Head>
+        <Herobanner />
+        <Care />
+        <Service />
+        <AboutCare />
+        <Features />
+        <Appointment />
+      </div>
+    </Suspense>
   );
 }
